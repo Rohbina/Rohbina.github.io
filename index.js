587b773d914bb7cc9da14514f1ca6aec36d90dcd
Robin Iongi <rohbina1980@gmail.com>
2024-11-08 21:17:00 -0700

    robin's final website

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ greeting.style.fontFamily = "cursive";
 
 const username = window.prompt("What's your name?", "Wonder Woman");
 greeting.innerHTML = "Welcome to my website, " + username + "!";
+
+let currentIndex = 0;
+
+function moveSlide(step) {
+  const images = document.querySelectorAll(".carousel-images img");
+  const totalImages = images.length;
+
+  currentIndex = (currentIndex + step + totalImages) % totalImages;
+
+  const carousel = document.querySelector(".carousel-images");
+  carousel.style.transform = `translateX(-${currentIndex * 100}%)`;
+}
+
+function autoPlay() {
+  moveSlide(1);
+  setTimeout(autoPlay, 3500);
+}
+
+window.onload = autoPlay;
